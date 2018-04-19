a57747a49371f085658cc522273f07b69545a2da
german20 <german20ymail.com>
2018-04-19 11:17:28 -0400

    Cambios para detalles de la ubicacion de un mapa

---
+++ sass.config.js
@@ -0,0 +1,7 @@
+module.exports = { 
+    includePaths: [ "node_modules/ionic-angular/themes", 
+                    "node_modules/ionicons/dist/scss", 
+                    "node_modules/ionic-angular/fonts", 
+                    "node_modules/bootstrap/scss/" 
+                ] 
+};
---
+++ src/pages/detalle/detalle.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+import { NavController } from 'ionic-angular';
+
+@Component({
+  selector: 'page-detalle',
+  templateUrl: 'detalle.html',
+})
+export class DetallePage {
+
+  constructor(public navCtrl: NavController) {
+    this.navCtrl = navCtrl;
+  }
+
+  
+
+}
