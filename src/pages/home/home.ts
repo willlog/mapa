2fb62bc12360c19205dad17724c91aa6c362e418
german20 <german20ymail.com>
2018-04-26 16:00:47 -0400

    Agrego cambios para conexion con tiles locales de ambato y cambion la forma de llamar a detalles de puntos

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { ModalController} from 'ionic-angular';
+
+import {DetallePage} from '../detalle/detalle';
 import  * as Leaflet from 'leaflet';
 import 'leaflet-draw';
 
@@ -14,34 +16,35 @@ export class HomePage {
   data:any;
   estado:any;
 
-  constructor(public navCtrl: NavController) {
-
+  constructor(public modalCtrl: ModalController) {
   }
 
+  
   ngOnInit():void{
-    this.drawMap();
+    this.drawMap(this.modalCtrl);
   }
 
-  drawMap(): void {
-    this.map = Leaflet.map('map').setView([-1.2490800, -78.6167500], 2);
-    Leaflet.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+  drawMap(d:any): void {
+    this.map = Leaflet.map('map').setView([-1.26381445,-78.61347275], 14);
+    Leaflet.tileLayer('assets/Tiles/{z}/{x}/{y}.png', {
       attribution: 'AMW',
-      maxZoom: 16
+      maxZoom: 18
     }).addTo(this.map);
     var map = this.map;
     
     map.locate({ setView: true});
 
-    function onLocationFound(e) {
+
+    /*function onLocationFound(e) {
       var radius = e.accuracy / 2;
 
       Leaflet.marker(e.latlng).addTo(map)
           .bindPopup("Estás dentro de los " + radius + "metros desde este punto").openPopup();
 
       Leaflet.circle(e.latlng, radius).addTo(map);
-    }
+    }*/
 
-    map.on('locationfound', onLocationFound);
+    //map.on('locationfound', onLocationFound);
 
 
     function addMarker(e) {
@@ -60,18 +63,20 @@ export class HomePage {
       L.geoJson(geojsonFeature, {
           
           pointToLayer: function(feature, latlng){
-              
               marker = L.marker(e.latlng, {
-                  
                   title: "Resource Location",
                   alt: "Resource Location",
                   riseOnHover: true,
                   draggable: true,
-  
-              }).bindPopup("<input type='button' value='Delete this marker' class='marker-delete-button'/>");
-  
+              }).bindPopup("<input type='button' value='Detalles' class='btn marker-delete-button'/>");
               marker.on("popupopen", onPopupOpen);
-         
+
+              marker.on("drag", function(e) {
+                  var marker = e.target;
+                  var position = marker.getLatLng();
+                  console.log(position);
+                  map.panTo(new L.LatLng(position.lat, position.lng));
+              });
               return marker;
           }
       }).addTo(map);
@@ -79,13 +84,28 @@ export class HomePage {
 
     function onPopupOpen() {
 
+   
       var tempMarker = this;
-  
+      console.log("Punto");
+      console.log(tempMarker);
       var marca = document.getElementsByClassName("marker-delete-button");
 
       for(var x = 0; x < marca.length; x++) {
         marca[x].addEventListener("click",function(){
-          map.removeLayer(tempMarker);
+
+          let profileModal = d.create(DetallePage, { callback: tempMarker });
+          profileModal.onDidDismiss(data => {
+            if(data){
+              if(data.option===0){
+                map.removeLayer(tempMarker);
+              }
+            }
+            
+
+          });
+          profileModal.present();
+          
+         
         },false);
       }
     }
@@ -93,13 +113,16 @@ export class HomePage {
     map.on('click', addMarker);
 
 
-    function onLocationError(e) {
+    /*function onLocationError(e) {
       alert(e.message);
     }
 
-    this.map.on('locationerror', onLocationError);
+    this.map.on('locationerror', onLocationError);*/
     
-      
   }
+
+  onPageWillEnter(){
+   alert("s");
+  } 
     
 }
